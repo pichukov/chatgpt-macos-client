bc8e6794946417c7ec0378d2817d94322e894266
Alexey Pichukov <pichukov@gmail.com>
2023-03-29 16:17:28 +0300

    Initial Commit

---
+++ ChatGPTClient/ChatGPTClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTClientApp.swift
+//  ChatGPTClient
+//
+//  Created by Alexey Pichukov on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
